bfcdfc64ac047746be9bff6f743d1c3be8d9dee3
Anthony Limani <alimani9193@gmail.com>
2022-03-15 10:46:12 -0400

    Add login/logout/signup functionality and corresponding views.

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -5,25 +5,27 @@ from . import views
 
 app_name = 'djangoapp'
 urlpatterns = [
-    # route is a string contains a URL pattern
-    # view refers to the view function
-    # name the URL
+                  # route is a string contains a URL pattern
+                  # view refers to the view function
+                  # name the URL
 
-    # path for about view
-    path(route='about', view=views.about, name='about'),
+                  # path for about view
+                  path(route='about', view=views.about, name='about'),
 
-    # path for contact us view
-    path (route='contact', view=views.contact, name='contact'),
-    # path for registration
+                  # path for contact us view
+                  path(route='contact', view=views.contact, name='contact'),
+                  # path for registration
+                  path(route='registration', view=views.registration_request, name='registration'),
+                  # path for login
+                  path(route='login', view=views.login_request, name='login'),
 
-    # path for login
+                  # path for logout
+                  path(route='logout', view=views.logout_request, name='logout'),
 
-    # path for logout
+                  path(route='', view=views.get_dealerships, name='index'),
 
-    path(route='', view=views.get_dealerships, name='index'),
+                  # path for dealer reviews view
 
-    # path for dealer reviews view
+                  # path for add a review view
 
-    # path for add a review view
-
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
