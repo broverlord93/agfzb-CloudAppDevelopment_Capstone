4b09085557ea01f3358f4cfebdd337fa0e362ae8
Anthony Limani <alimani9193@gmail.com>
2022-03-23 08:37:13 -0400

    Add REST endpoints and views to project.

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -5,27 +5,15 @@ from . import views
 
 app_name = 'djangoapp'
 urlpatterns = [
-                  # route is a string contains a URL pattern
-                  # view refers to the view function
-                  # name the URL
-
-                  # path for about view
-                  path(route='about', view=views.about, name='about'),
-
-                  # path for contact us view
-                  path(route='contact', view=views.contact, name='contact'),
-                  # path for registration
-                  path(route='registration', view=views.registration_request, name='registration'),
-                  # path for login
-                  path(route='login', view=views.login_request, name='login'),
-
-                  # path for logout
-                  path(route='logout', view=views.logout_request, name='logout'),
-
-                  path(route='', view=views.get_dealerships, name='index'),
-
-                  # path for dealer reviews view
-
-                  # path for add a review view
-
-              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    # route is a string contains a URL pattern
+    # view refers to the view function
+    # name the URL
+    path(route='about', view=views.about, name='about'),
+    path(route='contact', view=views.contact, name='contact'),
+    path(route='registration', view=views.registration_request, name='registration'),
+    path(route='login', view=views.login_request, name='login'),
+    path(route='logout', view=views.logout_request, name='logout'),
+    path(route='dealerships', view=views.get_dealerships, name='dealerships'),
+    path(route='dealer/<int:dealer_id/>', view=views.get_dealer_details, name='dealer_details'),
+    # path(route='review/add>', view=views.get_dealer_details, name='index'),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -10,28 +10,21 @@ from datetime import datetime
 import logging
 import json
 
-# Get an instance of a logger
+from server.djangoapp.restapis import get_dealers_from_cf
+from server.djangoapp.restapis import get_dealer_reviews_from_cf
+
 logger = logging.getLogger(__name__)
 
-
-# Create your views here.
-
-
-# Create an `about` view to render a static about page
 def about(request):
     context = {}
     if request.method == "GET":
         return render(request, 'djangoapp/about.html', context)
 
-
-# Create a `contact` view to return a static contact page
 def contact(request):
     context = {}
     if request.method == "GET":
         return render(request, 'djangoapp/contact.html', context)
 
-
-# Create a `login_request` view to handle sign in request
 def login_request(request):
     context = {}
     # Handles POST request
@@ -51,8 +44,6 @@ def login_request(request):
     else:
         return render(request, 'djangoapp/login.html', context)
 
-
-# Create a `logout_request` view to handle sign out request
 def logout_request(request):
     # Get the user object based on session id in request
     print("Log out the user `{}`".format(request.user.username))
@@ -61,8 +52,6 @@ def logout_request(request):
     # Redirect user back to course list view
     return redirect('djangoapp:index')
 
-
-# Create a `registration_request` view to handle sign up request
 def registration_request(request):
     context = {}
     # If it is a GET request, just render the registration page
@@ -86,23 +75,35 @@ def registration_request(request):
         # If it is a new user
         if not user_exist:
             # Create user in auth_user table
-            user = User.objects.create_user(username=username, first_name=first_name, last_name=last_name,
-                                            password=password)
+            user = User.objects.create_user(
+                username=username,
+                first_name=first_name,
+                last_name=last_name,
+                password=password
+            )
             # Login the user and redirect to course list page
             login(request, user)
             return redirect("djangoapp:index")
         else:
             return render(request, 'djangoapp/registration.html', context)
 
-# Update the `get_dealerships` view to render the index page with a list of dealerships
 def get_dealerships(request):
-    context = {}
     if request.method == "GET":
-        return render(request, 'djangoapp/index.html', context)
+        url = "https://ed9eb290.us-south.apigw.appdomain.cloud/api/dealership"
+        # Get dealers from the URL
+        dealerships = get_dealers_from_cf(url)
+        # Concat all dealer's short name
+        dealer_names = ' '.join([dealer.short_name for dealer in dealerships])
+        # Return a list of dealer short name
+        return HttpResponse(dealer_names)
 
-# Create a `get_dealer_details` view to render the reviews of a dealer
-# def get_dealer_details(request, dealer_id):
-# ...
+def get_dealer_details(request, dealer_id):
+    if request.method == 'GET':
+        url = "https://ed9eb290.us-south.apigw.appdomain.cloud/api/review"
+        reviews = get_dealer_reviews_from_cf(url, dealer_id)
+        review_text = [].append([review.review for review in reviews])
+        return HttpResponse(review_text)
+
 
 # Create a `add_review` view to submit a review
 # def add_review(request, dealer_id):
